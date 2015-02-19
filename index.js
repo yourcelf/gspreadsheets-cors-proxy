0d0bc8deee992e72d593ec6e4f2cb038eff22614
Charlie DeTar <cfd@media.mit.edu>
2015-02-19 13:22:31 -0700

    Fix CORS handling for POST and DELETE

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var httpProxy = require('http-proxy');
 var http = require('http');
 
 var CORS_HEADERS = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'HEAD, POST, GET, PUT, PATCH, DELETE',
-  'Access-Control-Max-Age': '86400',
-  'Access-Control-Allow-Headers': "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'HEAD, POST, GET, PUT, PATCH, DELETE',
+  'access-control-max-age': '86400',
+  'access-control-allow-headers': "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization"
 };
 
 
@@ -13,15 +13,23 @@ var CORS_HEADERS = {
 //var proxy = httpProxy.createProxyServer({target: 'https://spreadsheets.google.com'});
 var proxy = httpProxy.createProxyServer();
 
-http.createServer(function(req, res) {
+https.createServer(function(req, res) {
   if (req.method === 'OPTIONS') {
+    // Respond to OPTIONS requests advertising we support full CORS for *
     res.writeHead(200, CORS_HEADERS);
     res.end();
     return
   }
+  // Remove our original host so it doesn't mess up Google's header parsing.
   delete req.headers.host;
   proxy.web(req, res, {
     target: 'https://spreadsheets.google.com:443',
     xfwd: false
+  });
+  // Add CORS headers to every other request also.
+  proxy.on('proxyRes', function(proxyRes, req, res) {
+    for (var key in CORS_HEADERS) {
+      proxyRes.headers[key] = CORS_HEADERS[key];
+    }
   });
 }).listen(process.env.PORT || 5000);
